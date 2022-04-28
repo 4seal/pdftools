973e37de4cc36add065a8cc3b2347483e4c8c2b2
Khalif <85556739+4seal@users.noreply.github.com>
2022-04-28 18:57:04 +0300

    Update feedback.py

--- plugins/dm/feedback.py
+++ plugins/dm/feedback.py
@@ -21,14 +21,14 @@ ADMINS=Config.ADMINS
 
 UCantUse = "For Some Reason You Can't Use This Bot 🛑"
 
-feedbackMsg = "[Write a feedback 📋](https://t.me/nabilanavabchannel/17?comment=10)"
+feedbackMsg = "[Write a feedback 📋](https://t.me/kippikbot)"
 
 button=InlineKeyboardMarkup(
         [
             [
                 InlineKeyboardButton(
                     "😉 Create your Own 😉",
-                    url="https://github.com/nabilanavab/ilovepdf"
+                    url="https://t.me/kippikbot"
                 )
             ]
        ]
